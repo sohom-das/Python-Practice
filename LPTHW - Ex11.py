0acd7e99b38db7ba54591d17b57bb056437a9074
sohom-das <37765527+sohom-das@users.noreply.github.com>
2021-09-01 13:29:30 +0100

    Create LPTHW - Ex11.py

---
+++ LPTHW - Ex11.py
@@ -0,0 +1,5 @@
+age = input("How old are you?")
+weight = input("How much do you weight?")
+height = input("How tall are you?")
+
+print("So, you are {} old, {} tall and {} heavy.".format(age,height,weight))
