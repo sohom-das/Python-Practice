f283c75d0d15649b4aa30e2589d5070376e7c30b
sohom-das <37765527+sohom-das@users.noreply.github.com>
2021-09-01 17:12:38 +0100

    Create LPTHW - Ex12.py

---
+++ LPTHW - Ex12.py
@@ -0,0 +1,11 @@
+from sys import *
+
+script, first, second = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+
+
+user_input = input("Enter your text: ")
+print(f"Your text is: {user_input}")
