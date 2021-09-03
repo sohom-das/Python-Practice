5bfc98aa0dfdfbe47c25291016b0330ca0c049b7
sohom-das <37765527+sohom-das@users.noreply.github.com>
2021-09-03 19:11:01 +0100

    Create Self_practice.py
    Returning two functions and calling them separately.

---
+++ Self_practice.py
@@ -0,0 +1,12 @@
+def sum_multiply(a,b):
+    add = a+b
+    multi = a*b
+
+    return add,multi
+
+
+a, b = 10, 20
+add, multi = sum_multiply(a,b)
+
+print("The addition of the two numbers is: ", add)
+print("The multiplication of the two numbers is: ", multi)
